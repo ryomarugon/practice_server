2ef96a8f14cb872b05f07fe294601a4fedc4acf0
Ryomarugon <murakami.ryoma@geechs.com>
2023-02-19 11:21:12 +0900

    Initial commit

---
+++ FiveDimentionalArray.php
@@ -0,0 +1,10 @@
+<?php
+$array=[$sns,$users];
+$sns=['Twitter','Line','TikTok','Facebook'];
+$users=['id','name','email'];
+
+$webService['sns'][0]['users'][0]['name'];
+
+
+
+?>
---
+++ PrimeNumber.php
@@ -0,0 +1,18 @@
+<?php
+for ($i = 1; $i <= 1000; $i++) {
+    $count = 0;
+    
+    for ($j = 1; $j <= $i; $j++) {
+        if ($i % $j == 0) {
+            $count += 1;
+        }
+    }
+    
+    if ($count == 2) {
+        echo $i ;
+        echo '<br>';
+    }
+}
+
+
+?>
